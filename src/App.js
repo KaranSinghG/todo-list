4ffacaa21cfefd44eb71badb6296826e73754f36
Karan Singh <37649078+KaranSinghG@users.noreply.github.com>
2024-09-16 18:52:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import InputItem from "./Component/InputItem";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <InputItem />
+    </div>
+  );
+}
---
+++ src/Component/InputItem.js
@@ -0,0 +1,3 @@
+export default function InputItem() {
+  return <div>item</div>;
+}
